44e94558bfb6ebb04b0c9edc74812f20cf2d21bc
sharif-minhaz <minhaz.rbs@gmail.com>
2025-03-20 12:28:44 +0600

    feat: update pos printing

--- pos.cjs
+++ pos.cjs
@@ -1,33 +1,34 @@
 const ThermalPrinter = require("node-thermal-printer").printer;
 const PrinterTypes = require("node-thermal-printer").types;
 
-const thermalPrint = ({ configData, salesItems, salesViewData }) => {
-	console.log("print thermal data: ", configData, salesItems, salesViewData);
+const printer = new ThermalPrinter({
+	type: PrinterTypes.EPSON, // Printer type: 'star' or 'epson'
+	// usb://EPSON/RP328
+	// interface: "//localhost//RP328", // Printer interface
+	interface: "printer:RP328",
+	// driver: nodePrinter,
+	characterSet: "PC437_USA", // Printer character set - default: SLOVENIA
+	lineCharacter: "-", // Set character for lines - default: "-"
 
-	const printer = new ThermalPrinter({
-		type: PrinterTypes.EPSON, // Printer type: 'star' or 'epson'
-		interface: "localhost//RT378", // Printer interface
-		characterSet: "PC437_USA", // Printer character set - default: SLOVENIA
-		lineCharacter: "=", // Set character for lines - default: "-"
-		options: {
-			timeout: 5000,
-		},
-	});
+	options: {
+		timeout: 5000,
+	},
+});
 
-	printer.setLineSpacing(30);
-
-	printer.alignCenter();
+const thermalPrint = (salesViewData, configData, salesItems) => {
 	printer.bold(true);
-	printer.setTextSize(0, 0);
+	printer.setTextSize(1, 1);
+	printer.alignCenter();
 	printer.println(configData?.domain?.name || "TBD");
 	printer.bold(false);
+	printer.setTextSize(0, 0);
 	printer.drawLine();
 
 	printer.alignLeft();
 	printer.setTextSize(0, 0);
-	printer.println(`Email  : ${configData?.domain?.email || "N/A"}`);
+	printer.println(`Email: ${configData?.domain?.email || "N/A"}`);
 	printer.setTextSize(0, 0);
-	printer.println(`Mobile : ${configData?.domain?.mobile || "N/A"}`);
+	printer.println(`Mobile: ${configData?.domain?.mobile || "N/A"}`);
 	printer.setTextSize(0, 0);
 	printer.println(`Address: ${configData?.domain?.address || "N/A"}`);
 	printer.drawLine();
@@ -35,78 +36,63 @@ const thermalPrint = ({ configData, salesItems, salesViewData }) => {
 	printer.alignCenter();
 	printer.bold(true);
 	printer.setTextSize(0, 0);
-	printer.println("Retail Invoice");
+	printer.println(`Retail Invoice ${salesViewData?.invoice || "N/A"}`);
 	printer.bold(false);
 	printer.drawLine();
 
-	printer.alignLeft();
-	printer.bold(true);
-	printer.setTextSize(0, 0);
-	printer.println(`Invoice    : ${salesViewData?.invoice || "N/A"}`);
-	printer.setTextSize(0, 0);
-	printer.println(`Created    : ${salesViewData?.created || "N/A"}`);
-	printer.setTextSize(0, 0);
-	printer.println(`Created By : ${salesViewData?.createdByName || "N/A"}`);
-	printer.bold(false);
-	printer.drawLine();
-
-	printer.alignCenter();
-	printer.bold(true);
-	printer.setTextSize(0, 0);
-	printer.println("BllTo");
-	printer.bold(false);
-	printer.drawLine();
-
-	printer.alignLeft();
-	printer.bold(true);
-	printer.setTextSize(0, 0);
-	printer.println(`Customer   : ${salesViewData?.customerName || "N/A"}`);
-	printer.setTextSize(0, 0);
-	printer.println(`Mobile     : ${salesViewData?.customerMobile || "N/A"}`);
-	printer.setTextSize(0, 0);
-	printer.println(`Address    : ${salesViewData?.customer_address || "N/A"}`);
-	printer.bold(false);
+	printer.setTypeFontB();
+	printer.tableCustom([
+		{ text: `Date: ${salesViewData?.created || "N/A"}`, align: "LEFT", width: 0.5 },
+		{ text: `${salesViewData?.customerName || "N/A"}`, align: "RIGHT", width: 0.5 },
+	]);
+	printer.tableCustom([
+		{ text: `Seller:${salesViewData?.createdByName || "N/A"}`, align: "LEFT", width: 0.5 },
+		{ text: `${salesViewData?.customerMobile || "N/A"}`, align: "RIGHT", width: 0.5 },
+	]);
 	printer.drawLine();
 
 	// Table Header
+	printer.setTypeFontB();
+	printer.bold(true);
 	printer.tableCustom([
-		{ text: "Name", align: "LEFT", width: 0.3 },
-		{ text: "QTY", align: "CENTER", width: 0.15 },
-		{ text: "Unit", align: "CENTER", width: 0.15 },
+		{ text: "Name", align: "LEFT", width: 0.5 },
+		{ text: "QTY", align: "CENTER", width: 0.1 },
 		{ text: "Price", align: "RIGHT", width: 0.2 },
 		{ text: "Total", align: "RIGHT", width: 0.2 },
 	]);
+	printer.bold(false);
+	printer.drawLine();
 
 	// Table Rows (Sales Items)
 	salesItems.forEach((item) => {
 		printer.tableCustom([
-			{ text: item?.item_name || "-", align: "LEFT", width: 0.3 },
-			{ text: item?.quantity?.toString() || "0", align: "CENTER", width: 0.15 },
-			{ text: item?.purchase_price?.toString() || "0.00", align: "CENTER", width: 0.15 },
+			{ text: item?.item_name || "-", align: "LEFT", width: 0.4 },
+			{ text: item?.quantity?.toString() || "0", align: "CENTER", width: 0.1 },
 			{ text: item?.sales_price?.toString() || "0.00", align: "RIGHT", width: 0.2 },
 			{ text: item?.sub_total?.toString() || "0.00", align: "RIGHT", width: 0.2 },
 		]);
 	});
 
 	printer.drawLine();
-
+	printer.newLine();
 	// Totals
 	printer.tableCustom([
 		{ text: "SubTotal", align: "LEFT", bold: true, width: 0.5 },
-		{ text: salesViewData?.sub_total || "0.00", align: "RIGHT", width: 0.5 },
+		{ text: salesViewData?.sub_total?.toFixed(2) || "0.00", align: "RIGHT", width: 0.5 },
 	]);
 	printer.tableCustom([
 		{ text: "Discount", align: "LEFT", bold: true, width: 0.5 },
-		{ text: salesViewData?.discount || "0.00", align: "RIGHT", width: 0.5 },
+		{ text: salesViewData?.discount?.toFixed(2) || "0.00", align: "RIGHT", width: 0.5 },
 	]);
 	printer.tableCustom([
 		{ text: "Total", align: "LEFT", bold: true, width: 0.5 },
-		{ text: salesViewData?.total || "0.00", align: "RIGHT", width: 0.5 },
+		{ text: salesViewData?.total?.toFixed(2) || "0.00", align: "RIGHT", width: 0.5 },
 	]);
 	printer.tableCustom([
 		{ text: "Receive", align: "LEFT", bold: true, width: 0.5 },
-		{ text: salesViewData?.payment || "0.00", align: "RIGHT", width: 0.5 },
+		{ text: salesViewData?.payment?.toFixed(2) || "0.00", align: "RIGHT", width: 0.5 },
 	]);
+	printer.drawLine();
 	printer.tableCustom([
 		{ text: "Due", align: "LEFT", bold: true, width: 0.5 },
 		{
